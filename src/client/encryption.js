c6bb6c0a15e3f595e5ddf1ca4bfb8cba1ed492dd
possible_triangle <fireflyerfighter@gmail.com>
2019-01-29 11:05:31 +0100

    added basic interface

--- src/client/encryption.js
+++ src/client/encryption.js
@@ -1,38 +1,3 @@
-<<<<<<< HEAD
-function sendMessage() {
-
-	let msg = document.getElementById("input").value;
-	document.getElementById("input").value = "";
-
-	if(msg) {
-
-		generateAESKey(function(key, iv) {
-
-			let enc = encryptCBC(msg, key, iv);
-			console.log("Encrypted: " + enc.msg);
-			console.log("Decrypted: " + decryptCBC(enc.msg, key, iv, enc.textLength));
-
-		});
-
-	} else {
-
-		console.log("Empty Message");
-
-	}
-
-=======
-window.onload = function() {
-
-	generateAESKey(function(key, iv) {
-
-		let enc = encryptCBC("Test", key, iv);
-		console.log(enc.msg);
-		console.log(decryptCBC(enc.msg, key, iv, enc.textLength));
-
-	});
-
-}
-
 /*
 	Encrypt a message with CBC
 
@@ -164,5 +129,4 @@ function cutToBytes(bytes, textLength) {
 
 	return newBytes;
 
->>>>>>> f38e80954a159b89bc901eeae89729d9b8445b70
 }
