54fa068bfa3f7c911748fa37126ab14dd3b380c0
possible_triangle <fireflyerfighter@gmail.com>
2019-02-05 10:11:44 +0100

    message storage

--- src/client/client.js
+++ src/client/client.js
@@ -1,7 +1,7 @@
-let rsaKeys = generateRSAKeys("sameString");
-
-let username = "testname";
-let longtext = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam aliquam, orci at sollicitudin semper, lectus ante finibus diam, at pulvinar velit justo eu nisl. Integer pellentesque et ex sit amet tempus. Integer eros libero, semper non erat eu, aliquam dignissim est. Nulla luctus risus nec erat fringilla, a mattis dolor scelerisque. Suspendisse felis eros, aliquet ac consequat quis, gravida at turpis. Donec ultricies sodales ullamcorper. Duis a porta lacus. Aliquam euismod mollis nunc, ac condimentum purus semper ut. Praesent molestie ante nec risus semper tincidunt. Morbi sit amet commodo magna. Curabitur tellus tellus, euismod sed libero quis, luctus lacinia tortor. Duis mattis efficitur porta. Etiam mollis aliquet ligula quis eleifend. Nam molestie varius gravida. Maecenas laoreet at dolor non dapibus. Duis a est vitae quam sagittis mollis eget sed lectus";
+password = "somepassword123"
+username = "testname";
+rsaKeys = generateRSAKeys(window.password);
+anonymous = false;
 
 let maxDis = 200;
 
@@ -9,16 +9,25 @@ window.onload = function() {
 
 	let msg = document.getElementById("input").focus();
 
-	//	Create some test messages
-	displayMessage(sentence(), username);
-	displayMessage("*Pulls out meat scepter*", username);
-	for(let i = 0; i < 2; i++)
-		displayMessage(someSentences(1, 2), "Franzl");
-	for(let i = 0; i < 3; i++)
-		displayMessage(someSentences(1, 4), "Heidi");
+	connect();
 
 	scrollToBottom(true);
-	//window.setTimeout(scrollToBottom, 1000);
+
+	let names = ["Heidi", "Franzl", "Shrek"];	
+	let sim = function() {
+		for(let i = 0; i < randomInt(1, 3); i++) { 
+			let min = randomInt(1, 3);
+			let max = min + randomInt(0, 2);
+			displayMessage(someSentences(min, max), names[randomInt(0, names.length-1)], + new Date());
+		}
+		window.setTimeout(sim, 1000*5*randomInt(1, 20));
+	}
+	//sim();
+
+	displayMessage("www.agar.io", "Test", + new Date());
+	displayMessage("🦄", "Test", + new Date());
+
+	loadMessages();
 
 }
 
@@ -28,26 +37,39 @@ window.onload = function() {
 	@param{String} text - the message text (obviously)
 	@param{String} byUser - the username of the sender
 */
-function displayMessage(text, byUser) {
+function displayMessage(text, byUser, timestamp, anon) {
 
 	//	Checks if the last message was sent by the same user
-	compressed = (window.lastUser == byUser);
+	let timeAgo = timestamp - window.lastTimeStamp;
+	let compressed = (window.lastUser == byUser);
+	let compressed2 = (timeAgo < 5 * 1000 * 6) && compressed;
 	window.lastUser = byUser;
+	window.lastTimeStamp = timestamp;
 
 	let msg = document.createElement("div");
 	let bubble = document.createElement("div");
 	let info = document.createElement("div");
+	let time = document.createElement("div");
 
 	msg.className = "message " + (compressed ? "compressed " : " ") + (byUser == username ? "right" : "left");
 	info.className = "info";
 	bubble.className = "bubble";
+	time.className = "timestamp";
 
-	bubble.innerText = text;
-	info.innerText = (byUser == username ? "You" : byUser) + ":";
+	time.innerText = dateString(timestamp);
+	if(anon)
+		info.innerHTML = (byUser == username ? "You" : "Anonymous<i style='margin-left:1%;' class='fas fa-mask'></i>") + ":";
+	else
+		info.innerText = (byUser == username ? "You" : byUser) + ":";
 
-	if(!compressed) {
+	for(let node of parseRawText(text))
+		bubble.appendChild(node);
+
+	if(!compressed)
 		msg.appendChild(info);
-	}	
+	if(!compressed2)
+		bubble.appendChild(time);
+
 	msg.appendChild(bubble);
 	document.getElementById("messages").appendChild(msg);
 
@@ -74,31 +96,19 @@ function sendMessage() {
 
 	if(msg) {
 
-		displayMessage(msg, username);
+		let timestamp = + new Date();
+
+		msg = parseMessage(msg);
+
+		displayMessage(msg, username, timestamp);
 		scrollToBottom(false);
 
-		generateAESKey(function(key, iv) {
-
-			let enc = encryptCBC(msg, key, iv);
-
-			let receiverKey = rsaKeys.public;
-
-			let encryptedKey = cryptico.encrypt(keyToString(key, iv), receiverKey).cipher;
-
-			//		Send ...
-
-			let decryptedKey = stringToKey(cryptico.decrypt(encryptedKey, rsaKeys.private).plaintext);
-
-			console.log("Decrypted: " + decryptCBC(enc.msg, decryptedKey.key, decryptedKey.iv, enc.textLength));
-
-		});
+		sendProtocol(msg, timestamp);
 
 		if(msg.toLowerCase().includes("ugly")) {
 			toggleStyle("ugly");
 		}
 
-	} else {
-		console.log("Empty Message");
 	}
 }
 
@@ -154,13 +164,15 @@ function stringToKey(string) {
 }
 
 function darkMode() {
-
 	for(let style of document.querySelectorAll('[rel="stylesheet"]'))
 		if(style.href.endsWith("dark.css")) {
 			style.disabled = !style.disabled;
 		}
+}
 
-
+function anon() {
+	window.anonymous = !window.anonymous;
+	document.getElementById("anon").style.background = window.anonymous ? "var(--btn-hover-color)" : "var(--btn-color)";
 }
 
 function toggleStyle(name) {
@@ -172,3 +184,78 @@ function toggleStyle(name) {
 			console.log(style);
 
 }
+
+function dateString(timestamp) {
+
+	let now = new Date();
+	let then = new Date(timestamp);
+	let minAgo = parseInt((now - then) / 1000 / 60);
+
+	if(minAgo == 0)
+		return "just now";
+
+	if(minAgo < 30)
+		return minAgo + " minutes ago";
+
+	let s = then.getDate() + "." + (then.getMonth()+1) + ".";
+	if(then.getFullYear() != now.getFullYear())
+		s += then.getFullYear();
+
+	return s;
+
+}
+
+function parseMessage(msg) {
+	msg = msg.replace(emojiregex, function(match) { return "@emoji[" + match.codePointAt(0) + ":" + match.codePointAt(1) + "]"; });
+
+	return msg;
+}
+
+function parseRawText(rawtext) {
+
+	let nodes = [];
+
+	let words = rawtext.split(/[ ,]+/);
+	for(let word of words) {
+
+		word = word.replace(/@emoji\[[0-99999]+:[0-99999]+\]/g, function(match) { let s = match.replace(/@emoji\[|\]/, "").split(":"); return String.fromCodePoint(s[0]); });
+		word = word.replace(/@emoji\[[0-99999]+\]/g, function(match) { let s = match.replace(/@emoji\[|\]/, ""); return String.fromCodePoint(s); });
+
+		var node = document.createElement("a");
+		node.innerText += word + " ";
+
+ 		if(word.match(urlregex)) {
+ 			url = word.startsWith("http") ? word : "http://" + word;
+ 			node.className = "url";
+ 			node.target = "_blank";
+ 			node.href = url;
+ 		}
+
+ 		if(words.length == 1) {
+	 		if(word.match(singleemoji))
+	 			node.className = "size2";
+
+	 		if(word.match(doubleemoji))
+	 			node.className = "size1";
+	 	}
+
+ 		nodes.push(node);
+	}
+
+	return nodes;
+
+}
+
+function openLink(link) {
+	window.open(link);
+}
+
+String.prototype.replaceAll = function(search, replacement) {
+    var target = this;
+    return target.split(search).join(replacement);
+};
+
+let emojiregex = /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|[\ud83c[\ude01\uddff]|\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|[\ud83c[\ude32\ude02]|\ud83c\ude1a|\ud83c\ude2f|\ud83c[\ude32-\ude3a]|[\ud83c[\ude50\ude3a]|\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/g;
+let singleemoji = /^(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|[\ud83c[\ude01\uddff]|\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|[\ud83c[\ude32\ude02]|\ud83c\ude1a|\ud83c\ude2f|\ud83c[\ude32-\ude3a]|[\ud83c[\ude50\ude3a]|\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])$/g;
+let doubleemoji = /^((?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|[\ud83c[\ude01\uddff]|\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|[\ud83c[\ude32\ude02]|\ud83c\ude1a|\ud83c\ude2f|\ud83c[\ude32-\ude3a]|[\ud83c[\ude50\ude3a]|\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])){2}$/g;
+let urlregex = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/g;
