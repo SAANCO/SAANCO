f2a42352536f8898a1f74d036685ab98cbef489a
Anton Mrosek <info@anjomro.de>
2019-02-12 10:13:36 +0100

    Began to implement simple server side logic (for both master and node), ping and simplest way for message

---
+++ src/master/masterLogic.c
@@ -0,0 +1,54 @@
+//
+// Created by anjomro on 12.02.19.
+//
+
+#include "masterLogic.h"
+#include "wsserver.h"
+#include <string.h>
+#include <jansson.h>
+
+
+
+void gotMsg(int fromID, char msg[4096]){
+    json_t *parsing, *type, *from, *to;
+    json_error_t error;
+    parsing = json_loads(msg, 0, &error);
+
+    if(!parsing)
+    {
+        fprintf(stderr, "error: on line %d: %s\n", error.line, error.text);
+        return;
+    }
+    if(!json_is_object(parsing))
+    {
+        fprintf(stderr, "error: root is not an object\n");
+        json_decref(parsing);
+        return;
+    }
+    type = json_object_get(parsing, "type");
+    if(!json_is_integer(type)){
+        fprintf(stderr, "error: type is no int\n");
+        json_decref(parsing);
+        return;
+    }
+    json_int_t typeInt = json_integer_value(type);
+
+    switch (typeInt){
+        case 0:
+            //Ping
+            send_message((unsigned int) fromID, 1, msg);
+            break;
+        case 1:
+            //normal message
+            to = json_object_get(parsing, "to");
+            if(!json_is_integer(type)){
+                fprintf(stderr, "error: to is no int\n");
+                json_decref(parsing);
+                return;
+            }
+            send_message((
+            unsigned int) json_integer_value(to), 1, msg);
+            break;
+    }
+
+}
---
+++ src/master/masterLogic.h
@@ -0,0 +1,8 @@
+//
+// Created by anjomro on 12.02.19.
+//
+
+#ifndef SAANCO_MASTERLOGIC_H
+#define SAANCO_MASTERLOGIC_H
+
+#endif //SAANCO_MASTERLOGIC_H
